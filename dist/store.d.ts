19f594cd8a25f7d556a8c5059fc3e5ebf0137ca6
Sven Mielke <sven.mielke@inside-m2m.de>
2016-03-29 15:52:09 +0200

    build

--- dist/store.d.ts
+++ dist/store.d.ts
@@ -1,48 +1,18 @@
 import { Observable } from 'rxjs/Observable';
 import { BehaviorSubject } from 'rxjs/subject/BehaviorSubject';
-import { Subject } from 'rxjs/Subject';
-export interface Action {
-    type: string;
-    payload?: any;
-}
-export interface Reducer<T> {
-    (state: T, action: Action): T;
-}
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/distinctUntilChanged';
+import { Action, Reducer } from './interfaces';
+import { StoreBackend } from './store-backend';
+import { Dispatcher } from './dispatcher';
 export declare class Store<T> extends BehaviorSubject<T> {
     private _dispatcher;
-    private _reducers;
-    private _storeSubscription;
-    constructor(_dispatcher: Dispatcher<Action>, _reducers: {
-        [key: string]: Reducer<any>;
-    }, initialState: T);
-    /**
-     * It allows to add or update a reducer and its initial state on the fly
-     * It can be used when you need to load new parts of your store that are
-     * not available when the Store is created at startup
-     * @param reducer the function(s) to be added
-     * @param initialState the optional dictionary representing the state the new reducer
-     * should assume
-       */
-    replaceReducer(reducer: {
-        [key: string]: Reducer<any>;
-    }, initialState?: any): void;
-    private _mergeReducers(reducers, initialState);
+    private _backend;
+    constructor(_dispatcher: Dispatcher<Action>, _backend: StoreBackend, initialState?: T);
     select<R>(keyOrSelector: ((state: T) => R) | string | number | symbol): Observable<R>;
+    getState(): T;
     dispatch(action: Action): void;
-    createAction(type: string): (payload?: any) => void;
+    next(action: any): void;
+    error(error?: any): void;
+    replaceReducer<V>(reducer: Reducer<V>): void;
 }
-export declare class Dispatcher<Action> extends Subject<Action> {
-    dispatch(action: Action): void;
-}
-export declare const provideStore: (reducers: {
-    [key: string]: Reducer<any>;
-}, initialState?: {
-    [key: string]: any;
-}) => any[];
-export declare const createStore: (reducers: {
-    [key: string]: Reducer<any>;
-}, initialState?: {
-    [key: string]: any;
-}) => (dispatcher: Dispatcher<any>) => Store<{
-    [key: string]: any;
-}>;
